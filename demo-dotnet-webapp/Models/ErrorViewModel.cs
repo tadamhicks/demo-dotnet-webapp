5891731348cbbc96108e674403b8245405a2fcea
Adam Hicks <ahicks@morpheusdata.com>
2018-01-21 11:20:14 -0700

    Add project files.

---
+++ demo-dotnet-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demo_dotnet_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
